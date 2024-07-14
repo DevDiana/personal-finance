0fa132083f7317702bda717cccd8b8e1790d741d
Diana Regina <devdianaregina@gmail.com>
2024-07-13 23:57:36 -0300

    :sparkles: adiciona sass no projeto

--- src/app/pages/add-transaction/add-transaction.component.ts
+++ src/app/pages/add-transaction/add-transaction.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-transaction',
   templateUrl: './add-transaction.component.html',
-  styleUrls: ['./add-transaction.component.css']
+  styleUrls: ['./add-transaction.component.scss'],
 })
 export class AddTransactionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
