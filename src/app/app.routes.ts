ae4715776a93f0a827ab23132731d79e8c144dae
Diana Regina <devdianaregina@gmail.com>
2024-07-13 23:52:46 -0300

    :sparkles: adiciona componentes iniciais e estrutura para o app de finanças pessoais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
-import { HomeComponent } from './home/home/home.component';
-import { authorizedGuard } from './_guard/authorized.guard';
+import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './authentication/register/register.component';
 
 export const routes: Routes = [
---
+++ src/app/pages/add-transaction/add-transaction.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { AddTransactionComponent } from './add-transaction.component';
+
+describe('AddTransactionComponent', () => {
+  let component: AddTransactionComponent;
+  let fixture: ComponentFixture<AddTransactionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddTransactionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddTransactionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
